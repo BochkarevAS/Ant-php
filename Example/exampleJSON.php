ff62dd2aac9532082d5f8f5f0577a2865483cba8
BochkarevAS <bochkarev-vl@mail.ru>
2017-10-15 15:28:54 +1000

    Hello World!!!

--- Example/exampleJSON.php
+++ Example/exampleJSON.php
@@ -3,6 +3,20 @@
 use Classes\Currency\CountCurrency;
 use Classes\Currency\Service\CbrXMLDaily;
 
+$client = new Predis\Client([
+    'scheme' => 'tcp',
+    'host'   => '127.0.0.1',
+    'port'   => 6379,
+]);
+
+$client = new Predis\Client('tcp://127.0.0.1:6379');
+
+$client->set('message', 'Hello world');
+$value = $client->get('message');
+print($value);
+
+
+
 $countCurrency = new CountCurrency(new CbrXMLDaily());
 
 $price = 17;
