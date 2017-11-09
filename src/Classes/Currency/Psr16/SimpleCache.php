d7d3fb91c146208782e22dce59f9fb30e15e73e3
BochkarevAS <bochkarev-vl@mail.ru>
2017-11-09 12:29:45 +1000

    Hello World!!!

--- src/Classes/Currency/Psr16/SimpleCache.php
+++ src/Classes/Currency/Psr16/SimpleCache.php
@@ -3,29 +3,16 @@
 namespace Classes\Currency\Psr16;
 
 use Classes\Currency\Service\Reader;
+use Psr\SimpleCache\CacheInterface;
 
 class SimpleCache {
 
     private $pool;
     private $anotherPsr16class;
 
-    public function __construct(Reader $pool, AnotherPsr16class $anotherPsr16class) {
+    public function __construct(Reader $pool, CacheInterface $anotherPsr16class) {
         $this->pool = $pool;
         $this->anotherPsr16class = $anotherPsr16class;
-    }
-
-    public function setCache() {
-        $this->anotherPsr16class->set("key", $this->pool->readFile(), 300);
-
-
-    }
-
-    public function getCache() {
-
-        if (!empty($this->pool->readFile())) {
-            return $this->pool->readFile();
-        }
-        return null;
     }
 
 }
